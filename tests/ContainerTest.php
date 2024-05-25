5f5185c9ec79ab98d12c1789874fd7da9ccaee26
ratiry <ratiry@yandex.ru>
2024-05-25 15:02:57 +0300

    bg of 50 ep

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -6,3 +6,9 @@ test('example', function () {
     $result=$container->resolve("foo")[0]();
     expect($result)->toEqual("bar");
 });
+test('example2', function () {
+    $container=new Container;
+    $container->bind("foo",fn()=>"bar");
+    $result=$container->resolve("foo")[0]();
+    expect($result)->toEqual("barr");
+});
