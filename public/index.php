9bd9cc9eb8e6806b227c16a04e20c957157b0748
ratiry <ratiry@yandex.ru>
2024-05-25 11:56:03 +0300

    bg of 48 ep

--- public/index.php
+++ public/index.php
@@ -4,14 +4,10 @@ use Core\Router;
 use Core\Session;
 use Core\ValidationException;
 const BASE_PATH = __DIR__.'/../';
-
-spl_autoload_register(function ($class) {
-    $class=str_replace("\\","/",$class);
-    require base_path($class . '.php');
-});
+require BASE_PATH."/vendor/autoload.php";
 require BASE_PATH.'Core/functions.php';
-
 require base_path("bootstrap.php");
+
 $router=new Router();
 $routes = require base_path("routes.php");
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
