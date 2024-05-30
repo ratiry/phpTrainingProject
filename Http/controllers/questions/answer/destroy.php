4543460003ae60f9d098cc467d6f19ae5ebb401b
ratiry <ratiry@yandex.ru>
2024-05-30 20:24:27 +0300

    made destroying of answer

--- Http/controllers/questions/answer/destroy.php
+++ Http/controllers/questions/answer/destroy.php
@@ -1,2 +1,8 @@
 <?php
-
+use Core\App;
+$db=App::resolve("Core\Database");
+$question_id=$_POST["question_id"];
+$db->query("DELETE FROM answers WHERE id=:id",[
+  "id"=>$_POST["answer_id"]
+]);
+redirect("/question?id=$question_id");
